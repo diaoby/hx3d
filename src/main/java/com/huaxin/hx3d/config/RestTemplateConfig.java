31e569ac8979ceeeae8adcfff797dcf02f25d2e9
diaoby <183204622@qq.com>
2021-01-28 16:20:36 +0800

    resttemplate 加连接池

--- src/main/java/com/huaxin/hx3d/config/RestTemplateConfig.java
+++ src/main/java/com/huaxin/hx3d/config/RestTemplateConfig.java
@@ -3,16 +3,21 @@ package com.huaxin.hx3d.config;
 import com.huaxin.hx3d.filter.HttpServletRequestReplacedFilter;
 import com.huaxin.hx3d.interceptor.LoggingHttpRequestInterceptor;
 import com.huaxin.hx3d.messageconverter.HxMappingJackson2HttpMessageConverter;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 
 /**
@@ -23,14 +28,30 @@ import java.util.List;
  */
 @Configuration
 public class RestTemplateConfig {
+
     /**
-     *
+     * 服务器返回数据(response)的时间，超过该时间抛出read timeout
      */
-    private static final int READ_TIME_OUT = 5000;
+    private static final int SOCKET_TIME_OUT = 10000;
     /**
-     *
+     * 从连接池中获取连接的超时时间，超过该时间未拿到可用连接，
+     * 会抛出org.apache.http.conn.ConnectionPoolTimeoutException:
+     *  Timeout waiting for connection from pool
      */
-    private static final int CONNECT_TIME_OUT = 15000;
+    private static final int CONNECTION_REQUEST_TIME_OUT = 1000;
+    /**
+     * 连接上服务器(握手成功)的时间，超出该时间抛出connect timeout
+     */
+    private static final int CONNECT_TIME_OUT = 5000;
+
+    /**
+     * 连接池最大连接数
+     */
+    private static final int MAX_TOTAL = 200;
+    /**
+     * 默认最大路由
+     */
+    private static final int DEFAULT_MAX_PERROUTE = 100;
 
 
     @Bean
@@ -48,10 +69,35 @@ public class RestTemplateConfig {
      */
     @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(READ_TIME_OUT);
-        factory.setConnectTimeout(CONNECT_TIME_OUT);
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient());
         return  factory;
+    }
+
+    /**
+     * http链接池
+     * 服务器返回数据(response)的时间，超过该时间抛出read timeout
+     * @return
+     */
+    @Bean
+    public HttpClient httpClient() {
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+        //设置整个连接池最大连接数 根据自己的场景决定
+        connectionManager.setMaxTotal(MAX_TOTAL);
+        //路由是对maxTotal的细分
+        connectionManager.setDefaultMaxPerRoute(DEFAULT_MAX_PERROUTE);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setSocketTimeout(SOCKET_TIME_OUT) //服务器返回数据(response)的时间，超过该时间抛出read timeout
+                .setConnectTimeout(CONNECT_TIME_OUT)//连接上服务器(握手成功)的时间，超出该时间抛出connect timeout
+                .setConnectionRequestTimeout(CONNECTION_REQUEST_TIME_OUT)//从连接池中获取连接的超时时间，超过该时间未拿到可用连接，会抛出org.apache.http.conn.ConnectionPoolTimeoutException: Timeout waiting for connection from pool
+                .build();
+        return HttpClientBuilder.create()
+                .setDefaultRequestConfig(requestConfig)
+                .setConnectionManager(connectionManager)
+                .build();
     }
 
     /**
