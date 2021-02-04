c1c1f4d0585c503e508e7a611ac344f804ceda21
diaoby <183204622@qq.com>
2021-02-04 17:46:36 +0800

    ip端口可配置

--- web/hx-3dgis/src/ddmap/modules/root/manager/store/requestCreators.js
+++ web/hx-3dgis/src/ddmap/modules/root/manager/store/requestCreators.js
@@ -3,9 +3,19 @@ import ActionCreators from './actionCreators'
 //所有请求的axios 都放在这里
 export default class RequestCreators {
 
+    //园区后端服务---上线后需要修改对应ip和端口
+    static SERVIER = {
+        serverIp:'localhost',
+        serverPort:8082
+    };
+
+    static GET_URL = (api)=>{
+        return 'http://' + RequestCreators.SERVIER.serverIp +':' + RequestCreators.SERVIER.serverPort + api
+    }
+
     static TODO_LIST_GET_LIST_REQUEST= ()=>{
         return (dispatch)=>{
-            const url = 'http://localhost:8082/hx3d/web/home/energytest/getlist';
+            let url = RequestCreators.GET_URL('/hx3d/web/home/energytest/getlist');
             Request.get(url, 
                 { asyn: true }, 
                 (data)=>{
@@ -17,10 +27,10 @@ export default class RequestCreators {
         }
     }
 
-    //园区概况
+    //园区概况api
     static PARK_INFO_GET_INFO_REQUEST= ()=>{
         return (dispatch)=>{
-            const url = 'http://localhost:8082/hx3d//web/home/partinfo/getpartinfo';
+            let url = RequestCreators.GET_URL('/hx3d/web/home/partinfo/getpartinfo');
             Request.get(url, 
                 { asyn: true }, 
                 (data)=>{
